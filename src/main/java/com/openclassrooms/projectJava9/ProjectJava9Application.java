1fd4506253f14159a519a1dddb4e72a7ec387bf1
NdeyeHoudia <73912336+NdeyeHoudia@users.noreply.github.com>
2024-10-03 14:56:13 +0200

    nettoyer le ain

--- src/main/java/com/openclassrooms/projectJava9/ProjectJava9Application.java
+++ src/main/java/com/openclassrooms/projectJava9/ProjectJava9Application.java
@@ -1,44 +1,12 @@
 package com.openclassrooms.projectJava9;
 
-import com.openclassrooms.projectJava9.model.Patient;
-import com.openclassrooms.projectJava9.repository.PatientRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Optional;
 
 @SpringBootApplication
 public class ProjectJava9Application {
 
-//	private final Logger logger = LoggerFactory.getLogger(ProjectJava9Application.class);
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJava9Application.class, args);
 	}
-/*
-	@Override
-	public void run(String... args) throws Exception {
-	/*	Optional<Patient> patient = patientRepository.findById("66f2dfcf7dbb43d16f803347");
-			if(patient.isPresent()){
-				logger.info(patient.get().getId());
-			}else {
-				logger.info("Patient not found");
-			}
-
-
-		Patient patient = new Patient();
-		patient.setNom("test1");
-		patient.setPrenom("ada");
-		patient.setGenre("f");
-		patient.setAdresse("angers");
-		patient.setTelephone("77 77 77 77");
-		patientRepository.insert(patient);
-	}
- */
 }
